361ac80b1557b89e9ed3c1c82ff35fb498cc05b6
pwting1119 <71752445+pwting1119@users.noreply.github.com>
2020-10-21 15:37:13 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    for (let I = 0; I <= 4; I++) {
+        for (let J = 0; J <= 4; J++) {
+            if (I % 2 == J % 2) {
+                led.plot(I, J)
+            } else {
+            	
+            }
+        }
+    }
+})
